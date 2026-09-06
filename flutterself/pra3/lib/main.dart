cf93c9d743ca925a2ead72faaf2f1b100fbf16a6
Sayem2004 <mdalimransayem@gmail.com>
2026-09-06 16:18:50 +0600

    StatelessWidget and StatefulWidget

--- flutterself/pra3/lib/main.dart
+++ flutterself/pra3/lib/main.dart
@@ -12,27 +12,84 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello Fahad"),
-        ),
+        appBar: AppBar(title: const Text("Hello Fahad")),
         body: Column(
           children: [
-            Text("Hello Farhan Gunda",
+            Text(
+              "Hello Farhan Gunda",
               style: TextStyle(
                 fontSize: 25,
                 color: Colors.red,
                 fontWeight: FontWeight.bold,
               ),
             ),
+            Container(child: Text("Hello Hi")),
+            SizedBox(height: 15),
+
             Container(
-                child:
-                  Text("Hello Hi"),
+              color: Colors.red,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Button Clicked");
+                },
 
+                child: Text("Next"),
+              ),
             ),
+            SizedBox(height: 50),
+            UserName(name: "Sayem"),
 
+            const CounterScreen(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class UserName extends StatelessWidget {
+  final String name;
+
+  const UserName({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("User Name: $name");
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() {
+    return _CounterScreenState();}
+
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter =0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Counter: $counter"),
+        ElevatedButton(onPressed: (){
+          setState(() {
+            counter++;
+          });
+        },
+          child: const Text("Increase"),
+        ),
+        ElevatedButton(onPressed: (){
+          setState(() {
+            counter--;
+          });
+
+        },
+        child: const Text("Decrease"),),
+
+      ],
     );
   }
 }
